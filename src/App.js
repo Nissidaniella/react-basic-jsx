561566e7cc6efb3ea3bfa507de717f2ba8351ef2
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2023-11-08 01:19:45 +0000

    html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function GameWeb() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="nav">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Buy NFG</a></li>
+      <li><a href="#">Feeding</a></li>
+      <li><a href="#">FAQ</a></li>
+    </ul>
+   <button>Browse shop</button>
     </div>
+
+  <div class="head">
+    <h1>Nissi's Favorite <br></br>
+      <span>Games</span></h1>
+  </div>
+
+  <div id="body">
+    <div class="left">
+      <h3>Digital marketplace for crypto
+        <br></br>
+        collectibles and non-fungible tokens
+        <br></br>
+        (NFGs).Buy, sell, and discover
+        <br></br> 
+        exclusive digital assats.
+      </h3>
+
+      <button>Discover</button>
+
+      <h2>Available on the</h2>
+      <h1>App Store</h1>
+    </div>
+
+  <div class="middle">
+    <h1></h1>
+    <h2></h2>
+    <h3></h3>
+  </div>
+
+<div class="right">
+  <h1>42K +</h1>
+  <p>User Active</p>
+  <br></br>
+
+  <h2>8K+</h2>
+  <p>Artworks</p>
+  <br></br>
+
+  <h3>2K+</h3>
+  <p>Artist</p>
+
+  <div class='logos'>
+
+  </div>
+</div>
+
+<div id='foot'>
+<div class="left">
+  <h1>Top Sellers</h1>
+  <p> dash lines</p>
+  <h2>Black Shadows <br></br> 
+  <span>@Nissi Diallo</span> </h2>
+  
+  <p> line</p>
+  <h3>969 <br></br>
+  <span>Games for sale</span> </h3>
+
+</div>
+<div class="right">
+  <h1>Open Gallery <span> sign</span></h1>
+  <p> dash lines</p>
+  <h2>Remaining Time
+    <br></br> 
+    <span>20h : 50m : 10s</span> </h2>
+  <p> line</p>
+  <h3>Current Bid <br></br>
+  <span>22.00 symbol</span>  </h3>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
   );
 }
 
-export default App;
+export default GameWeb;
